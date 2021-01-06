23a4741f424f872a3fefdd227854b4d19a96da0d
chris foulkes <chris@foulk.es>
2021-01-06 16:02:50 +0000

    Added SignUpPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'login_page.dart';
+import 'sign_up_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,7 +21,10 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(visualDensity: VisualDensity.adaptivePlatformDensity),
       // 2
       home: Navigator(
-        pages: [MaterialPage(child: LoginPage())],
+        pages: [
+          MaterialPage(child: LoginPage()),
+          MaterialPage(child: SignUpPage())
+        ],
         onPopPage: (route, result) => route.didPop(result),
       ),
     );
